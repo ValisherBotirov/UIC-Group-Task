2e3a144972ee15807262420e0ae536974f890f19
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-07-25 14:47:06 +0500

    Create project files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+   
+  </div>
+</template>
+
+<script>
+
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
