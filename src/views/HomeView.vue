dbb0632ec7b818221cb364f7fa38201354c54f14
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-07-25 17:18:53 +0500

    I created a dashboard window. I brought the data from the Json server and rendered it to the form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-   
+    
+  </div>
+  <div class = "card">
+    <div class="card-header">
+      <h6 class="card-title">Employee Performance</h6>
+    </div>
+    <div class = "card-body">
+      <div class = "table-responsive">
+        <table class = "table table-hover align-middle">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>FullName</th>
+              <th>Departament</th>
+              <th>Position</th>
+              <th>Gender</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for = "user in users[0]" :key="user">
+              <td>{{user.id}}</td>
+              <td>{{user.fullName}}</td>
+              <td>{{user.departament}}</td>
+              <td>{{user.position}}</td>
+              <td>{{user.gender}}</td>
+              <td>{{user.age}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
-// import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from "axios"
 
 export default {
   name: 'HomeView',
-  components: {
-    // HelloWorld
-  }
+ 
+  data() {
+    return {
+      users : []
+    }
+  },
+  methods: {
+    
+  },
+  async mounted() {
+    const res = await axios.get("http://localhost:3000/users");
+    console.log(res.data);
+    this.users.push(res.data)
+    console.log(this.users);
+  },
 }
 </script>
