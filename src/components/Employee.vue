cd80589072f031564d8024f3ae0be3c3b36997c0
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-07-26 16:21:43 +0500

    I finished the page view of one employee

--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <h1>One Employee info page</h1>
+  <div class = "border mx-10 my-20 p-10">
+    <h1 class = "text-[30px]">Employee's Information</h1>
+    <div class = "icon "> </div>
     
-    <button class = "btn btn-danger" @click = "getParams">Paramsni ol</button>
-    
+    <p class = "border-b-10 mt-2 font-semibold"><span>Full Name</span> {{params.fullName}}</p>
+    <p class = "border-b-10 mt-2 font-semibold"><span>Departament</span> {{params.departament}}</p>
+    <p class = "border-b-10 mt-2 font-semibold"><span>Position</span> {{params.position}}</p>
+    <p class = "border-b-10 mt-2 font-semibold"><span>Gender</span> {{params.gender}}</p>
+    <p class = "border-b-10 mt-2 font-semibold"><span>Age</span> {{params.age}}</p>
+
+    <div class = "flex gap-2 ">
+      <button class="btn btn-primary">Edit</button>
+      <button class="btn btn-danger">Delete</button>
+    </div>
   </div>
 </template>
 
