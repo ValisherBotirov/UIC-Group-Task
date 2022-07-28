95d1e4289e8c718711077b10c5c8b9ade9acdcb9
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-07-28 13:09:32 +0500

    Departamentlar statistikasini ko'rsatuvchi componenet qo'shdim

---
+++ src/components/Diagrams.vue
@@ -0,0 +1,191 @@
+const styleScript=()=>{ }
+
+<template>
+  <div class="container">
+    <div id="time">
+      <div class="circle" style="--color: #ff2972">
+        <div class="dots h_dot" ref="departamentIT"></div>
+        <svg>
+          <circle cx="70" cy="70" r="70"></circle>
+          <circle cx="70" cy="70" r="70" id="hh"></circle>
+        </svg>
+        <div id="hours" class="text-black">
+          {{ itProsent }} % <br /><span class="text-[16px]">IT</span>
+        </div>
+      </div>
+      <div class="circle" style="--color: #fee800">
+        <div class="dots m_dot" ref="departamentM"></div>
+        <svg>
+          <circle cx="70" cy="70" r="70"></circle>
+          <circle cx="70" cy="70" r="70" id="mm"></circle>
+        </svg>
+        <div id="minutes" class="text-black">
+          {{ marketingProsent }} % <br /><span>Marketing</span>
+        </div>
+      </div>
+      <div class="circle" style="--color: #04fc43">
+        <div class="dots s_dot" ref="departamentA"></div>
+        <svg>
+          <circle cx="70" cy="70" r="70"></circle>
+          <circle cx="70" cy="70" r="70" id="ss"></circle>
+        </svg>
+        <div id="seconds" class="text-black">
+          {{ acountingProsent }} % <br /><span>Acounting</span>
+        </div>
+      </div>
+      <div class="ap">
+        <div id="ampm">AM</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const styleScript = (count1, count2, count3) => {
+  let hh = document.getElementById("hh");
+  let mm = document.getElementById("mm");
+  let ss = document.getElementById("ss");
+
+  let dotH = document.querySelector(".h_dot");
+  let dotM = document.querySelector(".m_dot");
+  let dotS = document.querySelector(".s_dot");
+
+  hh.style.strokeDashoffset = 440 - (440 * +count1 * 5) / 60;
+  mm.style.strokeDashoffset = 440 - (440 * +count2 * 5) / 60;
+  ss.style.strokeDashoffset = 440 - (440 * +count3 * 5) / 60;
+
+  dotH.style.transform = `rotate(${+count1 * 5 * 6}deg)`;
+  dotM.style.transform = `rotate(${+count2 * 5 * 6}deg)`;
+  dotS.style.transform = `rotate(${+count3 * 5 * 6}deg)`;
+};
+
+export default {
+  props: ["counts"],
+  data() {
+    return {
+      itProsent: "",
+      marketingProsent: "",
+      acountingProsent: "",
+    };
+  },
+  methods: {
+    addStyle() {
+      console.log(this.counts);
+      this.itProsent = Math.round((this.counts[0] * 100) / this.counts[3]);
+      this.marketingProsent = Math.round(
+        (this.counts[1] * 100) / this.counts[3]
+      );
+      this.acountingProsent = Math.round(
+        (this.counts[2] * 100) / this.counts[3]
+      );
+    },
+  },
+
+  mounted() {
+    styleScript(this.counts[0], this.counts[1], this.counts[2]);
+    this.addStyle();
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+.container {
+  width: 100%;
+  min-height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* background: #2f363e; */
+}
+
+#time {
+  display: flex;
+  gap: 40px;
+  color: #fff;
+}
+
+#time .circle {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#time .circle svg {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  transform: rotate(270deg);
+}
+
+#time .circle svg circle {
+  width: 100%;
+  height: 100%;
+  fill: transparent;
+  stroke: #191919;
+  stroke-width: 4px;
+  transform: translate(5px, 5px);
+}
+
+#time .circle svg circle:nth-child(2) {
+  stroke: var(--color);
+  stroke-dasharray: 440;
+}
+
+#time div {
+  position: absolute;
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: 500;
+}
+
+#time div span {
+  position: absolute;
+  transform: translate(-50%, 0px);
+  font-size: 0.5rem;
+  font-weight: 300;
+  letter-spacing: 0.1rem;
+  text-transform: uppercase;
+}
+
+#time .ap {
+  position: relative;
+  font-size: 1rem;
+  transform: translate(-20px);
+}
+
+.dots {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  z-index: 10;
+}
+
+.dots::before {
+  content: "";
+  position: absolute;
+  top: -3px;
+  width: 15px;
+  height: 15px;
+  background: var(--color);
+  border-radius: 50%;
+  box-shadow: 0 0 20px var(--color), 0 0 60px var(--color);
+}
+span {
+  font-size: 16px;
+  font-weight: 800;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,161 +1,162 @@
 <template>
-
-<div class = "flex gap-4 justify-end bg-slate-100 pt-4 px-5">
-  <div class ="flex gap-3 item-center">
-    <div class = "flex flex-col">
-      <p><i class="fa-solid fa-users"></i> &nbsp; {{allCount}}</p>
-      <p class = "text-[11px]">All Employee</p>
+  <div class="flex gap-4 justify-end bg-slate-100 pt-4 px-5">
+    <div class="flex gap-3 item-center">
+      <div class="flex flex-col">
+        <p><i class="fa-solid fa-users"></i> &nbsp; {{ allCount }}</p>
+        <p class="text-[11px]">All Employee</p>
+      </div>
+      <div class="">
+        <i class="fa-solid fa-chart-column text-4xl text-orange-500"></i>
+      </div>
     </div>
-    <div class="">
-      <i class="fa-solid fa-chart-column text-4xl text-orange-500"></i>
+    <div class="flex gap-3 item-center">
+      <div class="flex flex-col">
+        <p><i class="fa-solid fa-user-tie"></i> &nbsp; {{ maleCount }}</p>
+        <p class="text-[11px]">Male Employee</p>
+      </div>
+      <div class="">
+        <i class="fa-solid fa-chart-column text-4xl text-orange-500"></i>
+      </div>
+    </div>
+    <div class="flex gap-3 item-center">
+      <div class="flex flex-col">
+        <p><i class="fa-solid fa-user"></i> &nbsp; {{ famaleCount }}</p>
+        <p class="text-[11px]">Famale Employee</p>
+      </div>
+      <div class="">
+        <i class="fa-solid fa-chart-column text-4xl text-orange-500"></i>
+      </div>
     </div>
   </div>
-   <div class ="flex gap-3 item-center">
-    <div class = "flex flex-col">
-      <p><i class="fa-solid fa-user-tie"></i> &nbsp; {{maleCount}}</p>
-      <p class = "text-[11px]">Male Employee</p>
-    </div>
-    <div class="">
-      <i class="fa-solid fa-chart-column text-4xl text-orange-500"></i>
-    </div>
-  </div>
-   <div class ="flex gap-3 item-center">
-    <div class = "flex flex-col">
-      <p><i class="fa-solid fa-user"></i> &nbsp; {{famaleCount}}</p>
-      <p class = "text-[11px]">Famale Employee</p>
-    </div>
-    <div class="">
-      <i class="fa-solid fa-chart-column text-4xl text-orange-500"></i>
-    </div>
-  </div>
-</div>
 
   <div class="home grid grid-cols-[3fr_2fr] bg-slate-100 pt-4 px-5 gap-5">
-  <!-- Xodimlar ro'yxati -->
-     <div class = "card ">
+    <!-- Xodimlar ro'yxati -->
+    <div class="card">
       <div class="card-header bg-white border-b-none border-none">
-        <div class = "flex justify-between items-center bg-white pt-2">
+        <div class="flex justify-between items-center bg-white pt-3">
           <h6 class="card-title text-xl font-medium">Employee Information</h6>
-          <button class= "btn btn-success"><router-link to="/addUser" class="hover:text-white">Add Employee &nbsp;<i class="fa-solid fa-user-plus"></i></router-link></button>
+          <button class="btn btn-success">
+            <router-link to="/addUser" class="hover:text-white"
+              >Add Employee &nbsp;<i class="fa-solid fa-user-plus"></i
+            ></router-link>
+          </button>
         </div>
       </div>
-      <div class = "card-body ">
-        <div class = "table-responsive">
-          <table class = "table table-hover align-middle">
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-hover align-middle">
             <thead>
-              <tr class = "border-b border-[#f68c1f] border-b-2">
+              <tr class="border-b border-[#f68c1f] border-b-2">
                 <th>#</th>
                 <th>FullName</th>
                 <th>Departament</th>
                 <th>Position</th>
                 <th>Gender</th>
                 <th>Age</th>
-             </tr>
-           </thead>
-          <tbody>
-            <tr v-for = "(user,index) in users[0]" :key="user"  @click="toEmployee(user.id)">
-              <td class = "font-medium">{{index+1}}</td>
-              <td class="text-dark font-medium">{{user.fullName}}</td>              
-              <td class = "text-black">{{user.departament}}</td>
-              <td class = "text-black">{{user.position}}</td>
-              <td class="text-black">{{user.gender}}</td>
-              <td class="text-black">{{user.age}}</td>      
-            </tr>
-          </tbody>
-        </table>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(user, index) in users[0]"
+                :key="user"
+                @click="toEmployee(user.id)"
+              >
+                <td class="font-medium">{{ index + 1 }}</td>
+                <td class="text-dark font-medium">{{ user.fullName }}</td>
+                <td class="text-black">{{ user.departament }}</td>
+                <td class="text-black">{{ user.position }}</td>
+                <td class="text-black">{{ user.gender }}</td>
+                <td class="text-black">{{ user.age }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-     </div>
     </div>
 
     <!-- Kompaniya malumotlari -->
 
-   <div class = "border  p-10 h-full bg-white">
-    <h2 class = "center-text ">Kompaniya malumotlari</h2>
-     <p>Umumiy xodimlar soni : {{allCount}}</p>
-     <p>Erkak xodimlar soni : {{maleCount}}</p>
-     <p>Ayol xodimlar soni : {{famaleCount}}</p>
-     <p>IT departamentdagi xodimlar soni : {{itCount}}</p>
-     <p>Marketing departamentdagi xodimlar soni : {{marketingCount}}</p>
-     <p>Acounting departamentdagi xodimlar soni  : {{acountingCount}}</p>
-     <p>Xodimlarning o'rtacha yoshi : {{middleAge}}</p>
-   </div>
-  
-</div>
-
+    <div class="border p-3 h-full bg-white">
+      <h2 class="text-center text-xl font-medium">Employee Statistics</h2>
+      <p>Umumiy xodimlar soni : {{ allCount }}</p>
+      <p>Erkak xodimlar soni : {{ maleCount }}</p>
+      <p>Ayol xodimlar soni : {{ famaleCount }}</p>
+      <p>IT departamentdagi xodimlar soni : {{ itCount }}</p>
+      <p>Marketing departamentdagi xodimlar soni : {{ marketingCount }}</p>
+      <p>Acounting departamentdagi xodimlar soni : {{ acountingCount }}</p>
+      <p>Xodimlarning o'rtacha yoshi : {{ middleAge }}</p>
+      <Diagrams
+        v-if="allCount"
+        :counts="[itCount, marketingCount, acountingCount, allCount]"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from "axios"
-import Employee from "../components/Employee.vue"
-
-
-
+import axios from "axios";
+import Employee from "../components/Employee.vue";
+import Diagrams from "../components/Diagrams.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
 
-  components : {
-    Employee
+  components: {
+    Employee,
+    Diagrams,
   },
- 
+
   data() {
     return {
-      users : [],
-       maleCount : 0,
-      famaleCount : 0,
-      allCount : null,
-      itCount : 0,
-      marketingCount : 0,
-      acountingCount : 0,
-      middleAge : 0,
-      sumAge : 0  
-    }
+      users: [],
+      maleCount: 0,
+      famaleCount: 0,
+      allCount: null,
+      itCount: 0,
+      marketingCount: 0,
+      acountingCount: 0,
+      middleAge: 0,
+      sumAge: 0,
+    };
   },
-  
+
   methods: {
-      // Path bo'yicha chaqirib olish
-  toEmployee(id){
-    this.$router.push({path: `/emplyee/${id}`})
-  },
-    getOficeInfo(){
-       this.allCount = this.users[0].length
-       this.users[0].map(user => {
-         if(user.gender === "male"){
-          this.maleCount += 1
-         }
-    if(user.gender === "famale"){
-      this.famaleCount += 1
-    }
-    if(user.departament === "IT"){
-      this.itCount +=1
-      }
-    if(user.departament === "Marketing"){
-      this.marketingCount +=1
-    }
-    this.acountingCount = this.allCount - (this.marketingCount + this.itCount);
-    
-    this.sumAge = this.sumAge +(+user.age);
-    // console.log(this.sumAge);
-    
-    this.middleAge = Math.round(this.sumAge / this.allCount)
-   })
+    // Path bo'yicha chaqirib olish
+    toEmployee(id) {
+      this.$router.push({ path: `/emplyee/${id}` });
+    },
+    getOficeInfo() {
+      this.allCount = this.users[0].length;
+      this.users[0].map((user) => {
+        if (user.gender === "male") {
+          this.maleCount += 1;
+        }
+        if (user.gender === "famale") {
+          this.famaleCount += 1;
+        }
+        if (user.departament === "IT") {
+          this.itCount += 1;
+        }
+        if (user.departament === "Marketing") {
+          this.marketingCount += 1;
+        }
+        this.acountingCount =
+          this.allCount - (this.marketingCount + this.itCount);
 
-  }
- 
+        this.sumAge = this.sumAge + +user.age;
+        // console.log(this.sumAge);
 
+        this.middleAge = Math.round(this.sumAge / this.allCount);
+      });
+    },
   },
 
   async mounted() {
     const res = await axios.get("http://localhost:3000/users");
     this.users.push(res.data);
-    this.getOficeInfo()
+    this.getOficeInfo();
   },
-}
+};
 </script>
 
-<style>
-.icon{
-/* color : orange; */
-
-}
-</style>
+<style></style>
