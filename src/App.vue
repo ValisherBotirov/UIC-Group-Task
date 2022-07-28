cba900dc4ef8876dc36f8caf2fd6bea2dd934c8e
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-07-28 16:44:13 +0500

    AddView pageni dizaynini tugatdim

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
-<div class = "header border-b border-[#f68c1f] border-b-[3px] ">
-   <nav class = "p-[24px]">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/addUser">AddHuman</router-link>
-  </nav>
-</div>
- 
-  <router-view/>
+  <div class="header border-b border-[#f68c1f] border-b-[3px] bg-white">
+    <nav class="p-[24px]">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/addUser">AddHuman</router-link>
+    </nav>
+  </div>
+
+  <router-view />
 </template>
 
-
 <script>
-import RightMenu from "./components/RightMenu.vue"
-import HomeView from "./views/HomeView.vue"
+import RightMenu from "./components/RightMenu.vue";
+import HomeView from "./views/HomeView.vue";
 export default {
-  components : {
+  components: {
     RightMenu,
-    HomeView
-  }
-}
+    HomeView,
+  },
+};
 </script>
 
-
 <style>
-
 /* nav {
   padding: 24px;
 } */
+
+#app {
+  background-color: rgb(241 245 249);
+}
 
 nav a {
   font-weight: bold;
--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -1,95 +1,134 @@
 <template>
-  <div>
-    <form class = "form-control container pt-2 pb-10 px-6">
-      <h1 class = "text-center text-[30px] font-semibold">Add Employee</h1>
-      <label class = "py-2 font-medium">Full Name</label>
-      <input type="text" class = "placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" 
-      placeholder = "Enter Full Name" v-model = "fullName"/>
-      <label class = "py-2 font-medium">Departament</label>
-       <input
-          type="text"
-          class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-          placeholder="Enter Departament" v-model = "departament"
-        />
-        <label class = "py-2 font-medium">Position</label>
-        <input type="text" class = "placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-        placeholder = "Enter Position " v-model = "position"
-        />
-        <label class = "block py-2 font-medium">Choose gender : </label>
-        <div class = "flex gap-4">
-           <div class = "flex gap-2 items-center ml-5">
-             <label for="male" class = "font-medium">Male</label>
-             <input type="radio" name = "gender" id = "male" v-on:input = "getRadioAnsver" value="male" :checked="isChecked">
-           </div>
-           <div class = "flex gap-2 items-center ">
-             <label for="famale" class = "font-medium">Famale</label>
-             <input type="radio" name = "gender" id = "famale" v-on:input = "getRadioAnsver" value="famale" :checked="isChecked">
-           </div>
-         </div>
-         <label class = "py-2 font-medium">Age</label>
-         <input type="number" class = "placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-         placeholder = "Enter Age" v-model = "age"
-         />
-         <button type="submit" class = "bg-sky-500 rounded-md py-2 px-4 text-[white] mt-4" @click.prevent = "getUserInfo" >Save</button>
+  <div class="mt-5 container bg-slate-100">
+    <form class="addForm form-control container pt-4 pb-10 px-6">
+      <div class="flex justify-between pb-4">
+        <h1 class="text-[30px] font-semibold">Add Employee</h1>
+        <button class="btn btn-primary" @click.prevent="exitHome">
+          Exit &nbsp; <i class="fa-solid fa-right-from-bracket"></i>
+        </button>
+      </div>
+      <label class="py-2 font-medium">Full Name</label>
+      <input
+        type="text"
+        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+        placeholder="Enter Full Name"
+        v-model="fullName"
+      />
+      <label class="py-2 font-medium">Departament</label>
+      <input
+        type="text"
+        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+        placeholder="Enter Departament"
+        v-model="departament"
+      />
+      <label class="py-2 font-medium">Position</label>
+      <input
+        type="text"
+        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+        placeholder="Enter Position "
+        v-model="position"
+      />
+      <label class="block py-2 font-medium">Choose gender : </label>
+      <div class="flex gap-4">
+        <div class="flex gap-2 items-center ml-5">
+          <label for="male" class="font-medium">Male</label>
+          <input
+            type="radio"
+            name="gender"
+            id="male"
+            v-on:input="getRadioAnsver"
+            value="male"
+            :checked="isChecked"
+          />
+        </div>
+        <div class="flex gap-2 items-center">
+          <label for="famale" class="font-medium">Famale</label>
+          <input
+            type="radio"
+            name="gender"
+            id="famale"
+            v-on:input="getRadioAnsver"
+            value="famale"
+            :checked="isChecked"
+          />
+        </div>
+      </div>
+      <label class="py-2 font-medium">Age</label>
+      <input
+        type="number"
+        class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+        placeholder="Enter Age"
+        v-model="age"
+      />
+      <button
+        type="submit"
+        class="bg-sky-500 rounded-md py-2 px-4 text-[white] mt-4"
+        @click.prevent="getUserInfo"
+      >
+        Save &nbsp; <i class="fa-solid fa-message-plus"></i>
+      </button>
     </form>
   </div>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 export default {
-  name : "AddView",
+  name: "AddView",
   data() {
     return {
-      fullName : "",
-      departament : "",
-      position : "",
-      gender : "",
-      age : "",
-      newUser : null,
-      isChecked : null
-    }
+      fullName: "",
+      departament: "",
+      position: "",
+      gender: "",
+      age: "",
+      newUser: null,
+      isChecked: null,
+    };
   },
 
   methods: {
-   
-    getUserInfo(){
+    getUserInfo() {
       const newUser = {
-        fullName : this.fullName,
-        departament : this.departament,
-        position : this.position,
-        gender : this.gender,
-        age : +this.age
-      }
+        fullName: this.fullName,
+        departament: this.departament,
+        position: this.position,
+        gender: this.gender,
+        age: +this.age,
+      };
       this.newUser = newUser;
-        console.log(newUser);
-        this.getServer()
+      console.log(newUser);
+      this.getServer();
     },
-    async  getServer(){
-    console.log("Serverga jo'natdi");
-    await axios.post("http://localhost:3000/users",this.newUser,{})
-    //  await  axios.delete("http://localhost:3000/users/"+ this.id) 
-    // await axios.patch("http://localhost:3000/users/9",this.newUser)
-    this.clearInputValue()
-   },
-
-    clearInputValue(){
-      this.fullName = "",
-      this.departament = "",
-      this.position = "",
-      this.gender = "",
-      this.age = "",
-      this.isChecked = false
+    async getServer() {
+      console.log("Serverga jo'natdi");
+      await axios.post("http://localhost:3000/users", this.newUser, {});
+      //  await  axios.delete("http://localhost:3000/users/"+ this.id)
+      // await axios.patch("http://localhost:3000/users/9",this.newUser)
+      this.clearInputValue();
     },
 
-   getRadioAnsver(result){
-    this.gender = result.target.value
-   }
+    clearInputValue() {
+      (this.fullName = ""),
+        (this.departament = ""),
+        (this.position = ""),
+        (this.gender = ""),
+        (this.age = ""),
+        (this.isChecked = false);
+    },
+
+    getRadioAnsver(result) {
+      this.gender = result.target.value;
+    },
+    exitHome() {
+      this.$router.push({ path: "/" });
+    },
   },
-
-   
- 
-
-
-}
+};
 </script>
+
+<style scoped>
+.addForm {
+  width: 50%;
+}
+</style>
